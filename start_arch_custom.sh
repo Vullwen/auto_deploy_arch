13b05fc1c824f6849caa35ac6b20cba12a3af85d
Sajed BEN YOUSSEF <127627616+BySajed@users.noreply.github.com>
2025-02-14 23:27:57 +0100

    fix link

--- start_arch_custom.sh
+++ start_arch_custom.sh
@@ -284,7 +284,7 @@ EOF
     # 2) Installer pacman-contrib depuis l'AUR en tant que "collegue"
     arch-chroot /mnt runuser -u ${USER1} -- bash <<'EOCOL'
 cd /tmp
-git clone https://gitlab.archlinux.org/pacman/pacman-contrib-git.git
+git clone https://aur.archlinux.org/pacman-contrib-git.git
 cd pacman-contrib-git
 makepkg -si --noconfirm
 EOCOL
